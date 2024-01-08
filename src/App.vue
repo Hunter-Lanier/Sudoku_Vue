b94239943d87467b174773aeb8d156946b203049
hunterlanier.eth <hlanier97@gmail.com>
2024-01-07 22:24:44 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Grid from "./components/Grid.vue";
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <h1 class="text-5xl pb-10">Sudoku Solver</h1>
+    <Grid />
+    
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      v-for="(row, rowIndex) in sudokuBoard"
+      :key="rowIndex"
+      class="grid-row flex justify-between bg-slate-50 h-62 w-60"
+    >
+      <input
+        v-for="(cell, cellIndex) in row"
+        :key="cellIndex"
+        v-model="sudokuBoard[rowIndex][cellIndex]"
+        type="number"
+        min="1"
+        max="9"
+      />
+    </div>
+    <button @click="submitBoard">Submit</button>
+    <button @click="clearBoard">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sudokuBoard: Array.from({ length: 9 }, () => Array(9).fill("")),
+    };
+  },
+  methods: {
+    submitBoard() {
+      // Process or validate the Sudoku board here
+      console.log(this.sudokuBoard);
+    },
+    clearBoard() {
+      this.sudokuBoard = Array.from({ length: 9 }, () => Array(9).fill(""));
+    },
+    // Additional Sudoku methods...
+  },
+};
+</script>
+
+<style>
+sudokuBoard {
+}
+</style>
